aca6d6a30260ac065d4d968afd862b13faba786b
tones123 <tones123@gmail.com>
2013-09-03 16:04:37 +0800

    test

---
+++ jav/src/jav/Laptop.java
@@ -0,0 +1,19 @@
+package jav;
+
+class Laptop {
+
+	String memory = "1GB";
+}
+
+class Workshop {
+	public static void main(String args[]) {
+		Laptop life = new Laptop();
+		repair(life);
+		System.out.println(life.memory);
+	}
+
+	public static void repair(Laptop laptop) {
+		laptop = new Laptop();
+		laptop.memory = "2GB";
+	}
+}
